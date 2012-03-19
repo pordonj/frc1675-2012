2ea37b4152564d99809d7678389fa423a8a9e654
Josh <Josh@Josh-PC.gateway.2wire.net>
2012-03-18 22:22:03 -0500

    Major UPS2012 cleanup -- Mike and/or Trevor and/or someone else still needs to ddo cleanup on intake.

--- src/org/frc1675/ups2012/UPS2012Robot.java
+++ src/org/frc1675/ups2012/UPS2012Robot.java
@@ -8,19 +8,15 @@
 package org.frc1675.ups2012;
 
 
-import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.Ultrasonic.Unit;
-import edu.wpi.first.wpilibj.camera.*;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.frc1675.auton.*;
+import edu.wpi.first.wpilibj.*;
+import org.frc1675.auton.AutonomousScript;
 import org.frc1675.auton.BabbysFirstAutonomous;
-import org.frc1675.driver.JoystickTankController;
 import org.frc1675.driver.TankController;
 import org.frc1675.driver.XBoxController;
 import org.frc1675.driver.XBoxTankController;
 import org.frc1675.robot.component.SourceCounter;
 import org.frc1675.robot.component.Storage;
-import org.frc1675.robot.component.TwoMotorGearbox;
 import org.frc1675.robot.system.*;
 
 /**
@@ -31,11 +27,6 @@ import org.frc1675.robot.system.*;
  * directory.
  */
 public class UPS2012Robot extends IterativeRobot {
-    public static final double WHEEL_ENCODER_DPP = 0.0698;
-    
-    private static final int ULTRASONIC_SKIP_COUNT = 5;
-    private static final double ULTRASONIC_MIN_DISTANCE = 4.0;
-    private static final int ULTRASONIC_MIN_DETECT_COUNT = 20;
     
     //CRIO CARD SLOT DEFINITIONS
     private static final int DIGITAL_SIDECAR_1 = 1;
@@ -58,7 +49,7 @@ public class UPS2012Robot extends IterativeRobot {
     private static final int ULTRASONIC_2_INPUT = 3;
     private static final int ULTRASONIC_2_OUTPUT = 4;
     private static final int ULTRASONIC_3_INPUT = 5;
-    private static final int ULTRASONIC_3_OUTPUT = 6;
+    private static final int BALL_IN_LIMIT = 6;
     private static final int TURRET_ENCODER_A = 7;
     private static final int TURRET_ENCODER_B = 8;
     private static final int HOOD_ENCODER_A = 9;
@@ -72,11 +63,7 @@ public class UPS2012Robot extends IterativeRobot {
     private static final int ELEVATOR_A = 1;
     private static final int ELEVATOR_B = 2;
     private static final int BRIDGE_ARM = 3;
-
-    //vision IPs
-    private static final String VISION_CAMERA = "10.16.75.2";
-    private static final String INTAKE_CAMERA = "192.168.0.90";
-
+    
     //CONTROLLERS
     XBoxController driverController;
     XBoxController operatorController;
@@ -106,11 +93,16 @@ public class UPS2012Robot extends IterativeRobot {
      */
     public void robotInit() {
         //instantiate controllers
+        //PAD DRIVING
         driverController = new XBoxController(new Joystick(1));
+        operatorController = new XBoxController(new Joystick(2));
+        
+        //STICK DRIVING
 //        Joystick leftStick = new Joystick(1);
+//        operatorController = new XBoxController(new Joystick(2));
 //        Joystick rightStick = new Joystick(3);
 
-        operatorController = new XBoxController(new Joystick(2));
+        
         
         //instantiate DSC 1 PWM components
         SpeedController leftDrive = new Victor(DIGITAL_SIDECAR_1, LEFT_DRIVE);
@@ -132,20 +124,14 @@ public class UPS2012Robot extends IterativeRobot {
         Relay bridgeArmRelay = new Relay(DIGITAL_SIDECAR_1, BRIDGE_ARM, Relay.Direction.kBoth);
         
         //instantiate DSC 1 GPIO components
-//        Encoder leftEncoder = new Encoder(DIGITAL_SIDECAR_2, LEFT_ENCODER_A, DIGITAL_SIDECAR_2, LEFT_ENCODER_B, false);
-//        Encoder rightEncoder = new Encoder(DIGITAL_SIDECAR_2, RIGHT_ENCODER_A, DIGITAL_SIDECAR_2, RIGHT_ENCODER_B, false);
         shooterCounter = new Counter(DIGITAL_SIDECAR_1, SHOOTER_ENCODER_A);
-//      Encoder turretEncoder = new Encoder(DIGITAL_SIDECAR_1, TURRET_ENCODER_A, DIGITAL_SIDECAR_1, TURRET_ENCODER_B, false);
         Encoder hoodEncoder = new Encoder(DIGITAL_SIDECAR_1, HOOD_ENCODER_A, DIGITAL_SIDECAR_1, HOOD_ENCODER_B, true);
         DigitalInput hoodLimit = new DigitalInput(DIGITAL_SIDECAR_1, HOOD_LIMIT);
         DigitalInput armInLimit = new DigitalInput(DIGITAL_SIDECAR_1, BRIDGE_ARM_IN_LIMIT);
         DigitalInput armOutLimit = new DigitalInput(DIGITAL_SIDECAR_1, BRIDGE_ARM_OUT_LIMIT);
-        DigitalInput ballInLimit = new DigitalInput(DIGITAL_SIDECAR_1, ULTRASONIC_3_OUTPUT);
-        
-        //instantiate DSC 2 GPIO components
+        DigitalInput ballInLimit = new DigitalInput(DIGITAL_SIDECAR_1, BALL_IN_LIMIT);
         Ultrasonic ultraSonic1 = new Ultrasonic(DIGITAL_SIDECAR_1, ULTRASONIC_1_INPUT, DIGITAL_SIDECAR_1, ULTRASONIC_1_OUTPUT, Unit.kInches);
         Ultrasonic ultraSonic2 = new Ultrasonic(DIGITAL_SIDECAR_1, ULTRASONIC_2_INPUT, DIGITAL_SIDECAR_1, ULTRASONIC_2_OUTPUT, Unit.kInches);
-        //Ultrasonic ultraSonic3 = new Ultrasonic(DIGITAL_SIDECAR_1, ULTRASONIC_3_INPUT, DIGITAL_SIDECAR_1, ULTRASONIC_3_OUTPUT, Unit.kInches);
         
         
         
@@ -163,47 +149,66 @@ public class UPS2012Robot extends IterativeRobot {
         //ultraSonic3.setEnabled(true);
         
         //Instantiate sub-systems
+        
+        //DRIVE - PAD OR TANK
         TankController tankController = new XBoxTankController(driverController);
 //        TankController tankController = new JoystickTankController(leftStick, rightStick);
-//        Shooter shooter = new SteppingShooter(shooterMotor, operatorController);
-//        Shooter shooter = new VoltageShooter(shooterMotor);
-        Shooter shooter = new PIDShooter(shooterMotor, shooterCounter);
+        drive = new SimpleTankDrive(tankController, leftDrive, rightDrive, leftSideShifter, rightSideShifter);
+        
+        //TURRET
         turret = new SimpleTurret(turretMotor, operatorController);
-        //Hood hood = new SimpleHood(hoodMotor, operatorController);
+        
+        //SHOOTER SYSTEM
+        //- VOLTAGE PRESET FINDING
+//        Shooter shooter = new SteppingShooter(shooterMotor, operatorController);
+//        hood = new EncoderHood(hoodMotor, operatorController, hoodEncoder, hoodLimit);
+//        shooterSystem = new PresetFinderShooterSystem(shooter, hood, turret, driverController);
+        
+        
+        //- PID PRESET FINDING
+//        Shooter shooter = new SteppingShooter(shooterMotor);
+//        hood = new EncoderHood(hoodMotor, operatorController, hoodEncoder, hoodLimit);
+//        shooterSystem = new PresetFinderShooterSystem(shooter, hood, turret, driverController);
+        
+        //- VOLTAGE PRESET SHOOTING / ENCODER HOOD
+//        Shooter shooter = new VoltageShooter(shooterMotor);
+//        hood = new EncoderHood(hoodMotor, operatorController, hoodEncoder, hoodLimit);
+//        shooterSystem = new RotatingPresetShooterSystem(shooter, hood, turret, operatorController);
+        
+        //- PID PRESET SHOOTING / ENCODER HOOD
+        Shooter shooter = new PIDShooter(shooterMotor, shooterCounter);  
         hood = new EncoderHood(hoodMotor, operatorController, hoodEncoder, hoodLimit);
+        shooterSystem = new RotatingPresetShooterSystem(shooter, hood, turret, operatorController);
+        
+        //- PID PRESET SHOOTING / PID HOOD
+//        Shooter shooter = new PIDShooter(shooterMotor, shooterCounter);  
 //        PIDHoodSource hoodSource = new PIDHoodSource(hoodEncoder, hoodLimit);
 //        PIDHoodOutput hoodOutput = new PIDHoodOutput(hoodMotor, hoodLimit);
 //        hood = new PIDHood(hoodSource, hoodOutput);
+//        shooterSystem = new RotatingPresetShooterSystem(shooter, hood, turret, operatorController);
+
         
-//        shooterSystem = new ShooterSystem(shooter, turret, hood);
-//        shooterSystem = new PresetFinderShooterSystem(shooter, hood, turret, driverController);
-        shooterSystem = new RotatingPresetShooterSystem(shooter, hood, turret, operatorController);
+
+        //INTAKE AND STORAGE
+        //TODO clean up please
         UltrasonicWrapper ultraSonicWrapper1 = new UltrasonicWrapper(ultraSonic1, 8.0, 1);
         UltrasonicWrapper ultraSonicWrapper2 = new UltrasonicWrapper(ultraSonic2, 8.0, 1);
         //UltrasonicWrapper ultraSonicWrapper3 = new UltrasonicWrapper(ultraSonic3, 6.0, 5);
-
-        //instantiate camera
-//        AxisCamera visionCamera = AxisCamera.getInstance(VISION_CAMERA);
-//        visionCamera.writeCompression(0);
-//        visionCamera.writeResolution(AxisCamera.ResolutionT.k320x240);
-        //Instantiate systems
-        //drive = new EncoderTankDrive(tankController, leftGearbox, rightGearbox, leftSideShifter, rightSideShifter, leftEncoder, rightEncoder);
-        drive = new SimpleTankDrive(tankController, leftDrive, rightDrive, leftSideShifter, rightSideShifter);
+        
         Elevator elevator = new Elevator(leftLift,rightLift, operatorController);
         Intake intake = new Intake(intakeMotor, operatorController);
         ballStorage = new Storage(intake, elevator, -50, ultraSonicWrapper1, ultraSonicWrapper2, ballInLimit);
-//        shooterSystem = new ShooterSystem(shooter, turret, hood);
+        
+        //TODO end intake / storage cleanup
+        
+        //BRIDGE ARM
         bridgeArm = new SimpleBridgeArm(bridgeArmRelay, operatorController, armInLimit, armOutLimit);
-//        visionSystem = new VisionSystem(visionCamera);
         
-        
-        //Pick autonomous script
+        //AUTONOMOUS
         autonScript = new BabbysFirstAutonomous(shooterSystem, ballStorage);
         //autonScript = new FeedingScript(ballStorage, 6.0);
-        counter = new SourceCounter(shooterCounter);
         
-        SmartDashboardOutputProfiler.getInstance().setProfile("Driver");
-        SmartDashboardOutputProfiler.getInstance().putString("Driver","Title: ", "Welcome to Team 1675");
+        
     }
     
     
@@ -211,13 +216,7 @@ public class UPS2012Robot extends IterativeRobot {
      * Called at start of autonomous
      */
     public void autonomousInit(){
-        hoodReady = false;
-        reportTimer.start();
-        shooterCounter.start();
     }
-
-    
-    boolean hoodReady = false;
     /**
      * This function is called periodically during autonomous
      */
@@ -237,40 +236,17 @@ public class UPS2012Robot extends IterativeRobot {
 //        SmartDashboard.putInt("Shooter RPM", rpm);
     }
 
-    Timer reportTimer = new Timer();
-
     public void teleopInit(){
-//        shooterCounter.start();
-//        reportTimer.start();
-//        counter.start();
     }
 
-    long ticksPastSecond = 0;
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
         drive.driveTeleop();
-//        boolean shooting = shooterSystem.doTeleop();
         shooterSystem.doTeleop();
-        //ballStorage.doManual(false);
         ballStorage.manage(false);
         bridgeArm.doBridgeArm();
         turret.doTurret();
-//        if(reportTimer.get() > 1.0){
-//            ticksPastSecond = shooterCounter.get();
-//            shooterCounter.reset();
-//            reportTimer.reset();
-//        }
-//        int rpm = (int) (ticksPastSecond / 6L);
-//        SmartDashboard.putInt("Shooter RPM", rpm);
-//        SmartDashboard.putDouble("PID Get", counter.pidGet());
-//        SmartDashboard.putDouble("Enc Period", shooterCounter.getPeriod());
-    }
-
-    private int periodToRPM(double period) {
-        long ticksPerSec = (long)(1.0 / period);
-        long ticksPerMin = ticksPerSec * 60L;
-        return (int)(ticksPerMin / 360L);
     }
 }
